2d26993646a870dd07e7fe1df192e95ee2f9093f
yoshikik <yoshikik@kahokuzenki-no-MacBook-Air.local>
2015-02-02 13:58:19 +0900

    改行

--- aaaa/ViewController.h
+++ aaaa/ViewController.h
@@ -12,8 +12,8 @@
 {
     int number;//表示する画像の番号
     IBOutlet UIImageView *imgView ; //表示する画像
-    UIImage *imgArray[10] ; //表示する画像の配列（方法その１）
-    NSString *imgName[10] ; //表示する画像の名前の配列（方法その２）
+    //UIImage *imgArray[10] ; //表示する画像の配列（方法その１）
+    //NSString *imgName[10] ; //表示する画像の名前の配列（方法その２）
     
 }
 
